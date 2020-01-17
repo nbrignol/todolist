201a76872d95f6532e5ab8d854adf42cf7b5d936
Myself <you@example.com>
2020-01-17 09:48:33 +0100

    Use createElement to build list

---
+++ index.js
@@ -0,0 +1,33 @@
+var items = [
+	{
+		label: "Acheter des légumes"
+	},
+
+	{
+		label: "Faire du Javascript"
+	},
+
+	{
+		label: "Faire un peu de PHP"
+	}
+];
+
+var counterElement = document.querySelector(".counter .number");
+counterElement.innerHTML = items.length;
+
+var parentElement = document.querySelector(".items");
+parentElement.innerHTML = "";
+
+for(var i = 0; i < items.length; i++){
+	var newLi = document.createElement("li");
+	newLi.classList.add("item");
+	newLi.innerHTML = items[i].label;
+
+	var newA = document.createElement("a");
+	newA.setAttribute("href", "#")
+	newA.innerHTML = "Supprimer";
+
+	newLi.appendChild(newA);
+	parentElement.appendChild(newLi);
+
+}
